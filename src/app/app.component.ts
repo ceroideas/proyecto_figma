837ace9b8cde62542f4db93e7e35832c77ed60a4
CesarFarfan1995 <cesar95farfan@gmail.com>
2023-12-17 16:52:56 -0400

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { ComponentsModule } from './components/components.module';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ComponentsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    // Otras rutas aquí...
+    {
+      path: 'home',
+      component: HomeComponent,
+      children: [
+       
+       
+        // Puedes agregar más rutas hijas según sea necesario...
+      ]
+    },
+    // Otras rutas aquí...
+  ];
