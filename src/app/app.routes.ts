f216216d0c209ca2f02a7f7d0f666c722acc020c
CesarFarfan1995 <cesar95farfan@gmail.com>
2023-12-19 21:19:13 -0400

    add project page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 export const routes: Routes = [
-    // Otras rutas aquí...
-    {
-      path: 'home',
-      component: HomeComponent,
-      children: [
-       
-       
-        // Puedes agregar más rutas hijas según sea necesario...
-      ]
-    },
-    // Otras rutas aquí...
-  ];
+  { path: '', redirectTo: '/home/projects', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [{ path: 'projects', component: ProjectsComponent }],
+  },
+  // Otras rutas aquí...
+];
