2e7411b2c99f2b3b988060e3467209e5d20b2f28
CesarFarfan1995 <cesar95farfan@gmail.com>
2024-02-10 19:24:06 -0400

    cerate shape modal

--- src/app/components/shape-modal/shape-modal.component.ts
+++ src/app/components/shape-modal/shape-modal.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+declare var bootstrap: any;
 @Component({
   selector: 'app-shape-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './shape-modal.component.html',
-  styleUrl: './shape-modal.component.scss'
+  styleUrl: './shape-modal.component.scss',
 })
 export class ShapeModalComponent {
+  @ViewChild('shapeModal') miModal!: ElementRef;
+  clickedElement: number | null = null;
+  shapes: any[] = ['Normal', 'Normal', 'Normal', 'Normal'];
+  constructor() {}
 
+  ngAfterViewInit() {
+    const modal = new bootstrap.Modal(this.miModal.nativeElement);
+
+    modal._element.addEventListener('shown.bs.modal', () => {});
+
+    modal._element.addEventListener('hidden.bs.modal', () => {
+      console.log('jbhsjisb');
+      const openButton = document.querySelector('#exampleModalButton');
+
+      // Verifica si el botón existe antes de intentar cerrar el modal
+      if (openButton) {
+        // Simula un clic en el botón para cerrar el modal
+        (openButton as HTMLElement).click();
+      }
+    });
+  }
+
+  setClickedElement(index: number) {
+    this.clickedElement = index;
+  }
 }
