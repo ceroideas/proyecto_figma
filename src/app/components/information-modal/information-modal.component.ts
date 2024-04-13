a605141e20e3b866c846bf634d7703b9c2279137
CesarFarfan1995 <cesar95farfan@gmail.com>
2024-04-13 11:02:59 -0400

    fix

--- src/app/components/information-modal/information-modal.component.ts
+++ src/app/components/information-modal/information-modal.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-information-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './information-modal.component.html',
-  styleUrl: './information-modal.component.scss'
+  styleUrl: './information-modal.component.scss',
 })
 export class InformationModalComponent {
+  clickedElement: number = 0;
 
+  datas: any[] = [
+    { tier: 'L0', value: '15.325.896', description: 'Lorem lorem lorem' },
+    { tier: 'L0', value: '15.325.896', description: 'Lorem lorem lorem' },
+    { tier: 'L0', value: '15.325.896', description: 'Lorem lorem lorem' },
+    { tier: 'L0', value: '15.325.896', description: 'Lorem lorem lorem' },
+    { tier: 'L0', value: '15.325.896', description: 'Lorem lorem lorem' },
+    { tier: 'L0', value: '15.325.896', description: 'Lorem lorem lorem' },
+    { tier: 'L0', value: '15.325.896', description: 'Lorem lorem lorem' },
+    { tier: 'L0', value: '15.325.896', description: 'Lorem lorem lorem' },
+  ];
+
+  constructor() {}
+
+  setClickedElement(index: number) {
+    this.clickedElement = index;
+  }
 }
