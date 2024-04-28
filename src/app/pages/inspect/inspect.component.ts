3ac6e038e5e662dffe5ac79fabb2ae7711408b65
CesarFarfan1995 <cesar95farfan@gmail.com>
2024-04-27 21:32:09 -0400

    implemnet diferences in values

--- src/app/pages/inspect/inspect.component.ts
+++ src/app/pages/inspect/inspect.component.ts
@@ -29,34 +29,65 @@ export class InspectComponent implements OnInit {
   years: any[] = [];
   yearIndex: any[] = [];
 
+  valueToShow: any[] = [];
+  tierCeroValue: any = 0;
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private projectSvc: ProjectService
   ) {}
   ngOnInit(): void {
+    this.valueToShow = [];
     const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     this.id = this.route.snapshot.params['id'];
 
     this.projectSvc.getProject(this.id).subscribe((res: any) => {
-      this.nodes = res.nodes;
-      console.log(this.nodes);
+      this.nodes = res.nodes.map((node: any) => {
+        if (node.type === 2) {
+          node.calculated = node.calculated.map((obj: any, i: number) => {
+            obj.newName = abc.charAt(i);
+            return obj;
+          });
+        } else {
+          node.sceneries = node.sceneries.map((obj: any, i: number) => {
+            obj.newName = abc.charAt(i);
+            return obj;
+          });
+        }
+        return node; // Importante: devolver el nodo modificado
+      });
+
       const nodes = res.nodes.map((node: any) => {
         return {
           tier: 'L' + node.tier,
-          value: '15.325.896',
+          value: '0',
           description: node.name,
         };
       });
 
       this.datas = nodes.reverse();
-      this.tierCero = res.nodes
-        .find((node: any) => node.tier == 0)
-        .sceneries.map((obj: any, i: number) => {
-          obj.newName = abc.charAt(i);
 
-          return obj;
-        });
+      const node = res.nodes.find((node: any) => node.tier == 0);
+
+      if (node?.type === 2) {
+        this.tierCero = res.nodes
+          .find((node: any) => node.tier == 0)
+          .calculated.map((obj: any, i: number) => {
+            obj.newName = abc.charAt(i);
+
+            return obj;
+          });
+      } else {
+        this.tierCero = res.nodes
+          .find((node: any) => node.tier == 0)
+          .sceneries.map((obj: any, i: number) => {
+            obj.newName = abc.charAt(i);
+
+            return obj;
+          });
+      }
+
       let array: any[] = [];
       const keys = Object.keys(this.tierCero[0].years);
       for (let i = 0; i < keys.length; i++) {
@@ -74,14 +105,33 @@ export class InspectComponent implements OnInit {
         }
       }
 
+      console.log(array);
+
       const valoresAños: any[] = [];
 
       array.forEach((obj: any) => {
-        valoresAños.push(+obj.value);
+        this.valueToShow.push(obj.value);
+
+        const valueWithoutDecimals = obj.value.replace(/\.\d{2}$/, '');
+
+        // Eliminar tanto los puntos como las comas del valor
+        const cleanValue = valueWithoutDecimals.replace(/[,.]/g, '');
+
+        // Convertir el valor a un número
+        const numericValue = parseFloat(cleanValue);
+
+        // Verificar si el valor es un número válido
+        if (!isNaN(numericValue)) {
+          console.log(numericValue);
+          valoresAños.push(numericValue);
+        } else {
+          console.log('Valor no válido:', obj.value);
+        }
       });
       this.years = array;
 
       this.barData = valoresAños;
+      console.log(this.barData);
     });
   }
 
@@ -132,14 +182,60 @@ export class InspectComponent implements OnInit {
 
   calculatedNode() {
     if (this.yearIndex.length === 2) {
-      console.log([
+      const years = [
         this.years[this.yearIndex[0]],
         this.years[this.yearIndex[1]],
-      ]);
+      ];
 
+      let nodos: any = [];
       for (let i = 0; i < this.nodes.length; i++) {
-        const element = this.nodes[i];
+        const node = this.nodes[i];
+        let yearValues: any = [];
+        for (let i = 0; i < years.length; i++) {
+          const year = years[i];
+          if (node.type === 2) {
+            const scenerie: any = node.calculated.find(
+              (node: any) => node.newName == year.name
+            );
+            const yearValue = scenerie.years[`${year.year}`];
+            yearValues.push(yearValue);
+          } else {
+            const scenerie = node.sceneries.find(
+              (node: any) => node.newName == year.name
+            );
+            const yearValue = scenerie.years[`${year.year}`];
+            yearValues.push(yearValue);
+          }
+        }
+        yearValues.push('L' + node.tier);
+        yearValues.push(node.name);
+        nodos.push(yearValues);
       }
+
+      const diferencias = nodos
+        .map((par: any) => {
+          console.log(par);
+          const monto1 = parseFloat(par[0].replace(/,/g, ''));
+          const monto2 = parseFloat(par[1].replace(/,/g, ''));
+          return {
+            tier: par[2],
+            name: par[3],
+            value: Math.abs(monto1 - monto2),
+          };
+        })
+        .reverse();
+
+      this.tierCeroValue = diferencias.shift().value.toLocaleString('es-ES');
+
+      this.datas = diferencias.map((value: any) => {
+        return {
+          tier: value.tier,
+          value: value.value.toLocaleString('es-ES'),
+          description: value.name,
+        };
+      });
+
+      console.log(diferencias, 'nodos');
     }
   }
 
