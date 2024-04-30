cb3c8c33f4b9fc793cd427d328624429957880e9
Jorge Solano <jorgesolano92@gmail.com>
2024-05-01 00:28:48 +0200

    cambio en url de api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: 'https://andreapardini.art/figma/public/api',
+  url: 'http://165.227.102.141/api/public/api',
   /* url: 'http://127.0.0.1:8000/api', */
 };
 
