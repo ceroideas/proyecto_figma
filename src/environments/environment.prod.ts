2b418d6ed28acf71517dc1cc3447290713ed9c51
jorgeceroideas <jorge@ceroideas.es>
2024-10-17 22:05:21 +0200

    cambio url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  url: 'http://170.64.173.54/api/public/api',
+  // url: 'http://170.64.173.54/api/public/api',
+  url: 'http://209.38.31.107/api/public/api',
   /* url: 'http://127.0.0.1:8000', */
   secretKey: 'oKoWl7AqUWj8kYBKNdygwMW7ofrzjIIJM8MjBKCo66yvzhG8j0xs5rpKb8GOMFyk',
 };
